4ccb9e95e96f31384ca5d53c9a045633655ef278
Binh <vuongnguyenthanhbinh@gmail.com>
2024-04-25 13:56:54 +0700

    update limit

--- api/posts/index.ts
+++ api/posts/index.ts
@@ -4,7 +4,7 @@ import { Posts, Comment } from '@/base/types/posts';
 import api from '../api';
 
 export function fetchPosts(limit: string) {
-  return api.get(`/posts?_limit=${limit}}`);
+  return api.get(`/posts?_limit=${limit}`);
 }
 
 export function fetchDetailPost(id: string): Promise<AxiosResponse<Posts>> {
